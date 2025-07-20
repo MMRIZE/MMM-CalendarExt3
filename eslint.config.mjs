c51c2d9b6da1ba81b3cb9bb94a6eac288469cf7a
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-07-20 11:36:31 +0200

    Update linting setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,13 @@
-import globals from "globals"
-import pluginJs from "@eslint/js"
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import markdown from "@eslint/markdown";
 
-
-export default [
-  { files: [ "**/*.js" ], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.mjs"], languageOptions: {sourceType: "module"}},
-  { languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-]
+export default defineConfig([
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"], "rules": { "css/no-invalid-properties": "off", "css/no-empty-blocks": "off", "css/use-baseline": "off"} },
+  { files: ["**/*.{js,mjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { files: ["**/*.{js,mjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] }
+]);
