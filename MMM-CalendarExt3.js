1f246192419868f253822fbd35567a81276b18f1
Dennis Brookner <debrookner@gmail.com>
2026-07-31 13:03:13 -0400

    fix: update calendar on event notifications (#266)

--- MMM-CalendarExt3.js
+++ MMM-CalendarExt3.js
@@ -378,6 +378,7 @@ Module.register("MMM-CalendarExt3", {
     if (notification === this.notifications.eventNotification) {
       const convertedPayload = this.notifications.eventPayload(payload)
       this.eventPool.set(sender.identifier, structuredClone(convertedPayload))
+      this.updateAnimate()
     }
 
     if (notification === "MODULE_DOM_CREATED") {
